7b4e79a5062cfe2f3a04cae17eb024a9466e1c66
Egor Petrov <egor@barbatosso.com>
2020-11-06 18:12:30 +0300

    Initial commit

---
+++ MoneyHealth/MoneyHealth/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MoneyHealth
+//
+//  Created by Egor Petrov on 06.11.2020.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
