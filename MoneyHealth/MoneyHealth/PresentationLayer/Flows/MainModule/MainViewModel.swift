68e78e5d8cc16416fa459350c22451c4ecd3dc3c
Egor Petrov <egor@barbatosso.com>
2020-11-07 17:18:12 +0300

    View controller for main module

---
+++ MoneyHealth/MoneyHealth/PresentationLayer/Flows/MainModule/MainViewModel.swift
@@ -0,0 +1,64 @@
+//
+//  MainViewModel.swift
+//  MoneyHealth
+//
+//  Created by Egor Petrov on 07.11.2020.
+//
+
+import Foundation
+
+protocol AnyViewModelProtocol {
+    func viewLoaded()
+}
+
+extension AnyViewModelProtocol {
+    func viewLoaded() {}
+}
+
+protocol ViewModelProtocol: AnyViewModelProtocol {
+    associatedtype Input
+    associatedtype Output
+
+    var output: Self.Output { get }
+    var input: Self.Input { get }
+}
+
+protocol AnyControllerProtocol {
+    var anyViewModel: AnyViewModelProtocol { get }
+}
+
+protocol ControllerProtocol: AnyControllerProtocol {
+    associatedtype ViewModelType: ViewModelProtocol
+
+    var viewModel: ViewModelType { get }
+
+    func bindInput()
+    func bindOutput()
+}
+
+extension ControllerProtocol {
+    var anyViewModel: AnyViewModelProtocol {
+        return self.viewModel
+    }
+}
+
+final class MainViewModel: ViewModelProtocol {
+
+    struct Output {
+    }
+    
+    struct Input {
+    }
+
+    var output: Output
+    var input: Input
+
+    let collectionViewContainer: CollectionViewContainer
+
+    init() {
+        self.output = Output()
+        self.input = Input()
+
+        self.collectionViewContainer = CollectionViewContainer(dataSources: [])
+    }
+}
