097b32be275ce25514ee0e3a646fdabcf2dd2034
Egor Petrov <egor@barbatosso.com>
2020-11-07 19:26:32 +0300

    Add icons

---
+++ MoneyHealth/MoneyHealth/PresentationLayer/Flows/MainModule/TitleDataSource.swift
@@ -0,0 +1,95 @@
+//
+//  TitleDataSource.swift
+//  MoneyHealth
+//
+//  Created by Egor Petrov on 07.11.2020.
+//
+
+import UIKit
+
+final class TitleItemViewCell: UIView, GenericCellSubview {
+    
+    let titleLabel: UILabel = {
+       let label = UILabel()
+        label.font = .systemFont(ofSize: 22, weight: .bold)
+        label.textColor = .black
+        label.text = "Popular categories"
+        return label
+    }()
+
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+
+        self.setupInitialLayout()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    private func setupInitialLayout() {
+        self.addSubview(self.titleLabel)
+        self.titleLabel.snp.makeConstraints { make in
+            make.leading.equalToSuperview().offset(16)
+            make.top.equalToSuperview().offset(16)
+            make.bottom.equalToSuperview().inset(16)
+        }
+    }
+}
+
+class TitleDataSource: CollectionViewDataSource {
+    weak var delegate: CollectionViewDataSourceContainerDelegate?
+
+    var state = DataSourceState.items
+    var isEnabled = true
+    
+
+    var cellsForRegistration: [CollectionViewCell.Type]? {
+        return [GenericCollectionViewCell<TitleItemViewCell>.self]
+    }
+
+    var numberOfSections: Int {
+        return 1
+    }
+
+    func numberOfItems(inSection section: Int) -> Int {
+        return 1
+    }
+
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCellWithType(GenericCollectionViewCell<TitleItemViewCell>.self, indexPath: indexPath)
+        return cell
+    }
+
+    func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {}
+
+    func collectionView(
+        _ collectionView: UICollectionView,
+        layout collectionViewLayout: UICollectionViewLayout,
+        sizeForItemAt indexPath: IndexPath
+    ) -> CGSize {
+        return CGSize(width: collectionView.frame.width, height: 60)
+    }
+
+    func collectionView(
+        _ collectionView: UICollectionView,
+        layout collectionViewLayout: UICollectionViewLayout,
+        minimumLineSpacingForSectionAt section: Int
+    ) -> CGFloat {
+        return 0
+    }
+
+    func collectionView(
+        _ collectionView: UICollectionView,
+        layout collectionViewLayout: UICollectionViewLayout,
+        minimumInteritemSpacingForSectionAt section: Int
+    ) -> CGFloat {
+        return 0
+    }
+
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, insetForSectionAt section: Int) -> UIEdgeInsets {
+        return UIEdgeInsets(top: 40, left: 0, bottom: 0, right: 0)
+    }
+
+    func scrollViewDidScroll(_ scrollView: UIScrollView) {}
+}
