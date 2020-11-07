5d91689157d1bd35ec60d933b6ca2029dbe73aab
Alexander <azimin@me.com>
2020-11-07 22:19:55 +0300

    Added some logic

---
+++ MoneyHealth/MoneyHealth/PresentationLayer/Flows/SubscriptionDetailsModule/SubscriptionDetailsViewController.swift
@@ -0,0 +1,12 @@
+//
+//  SubscriptionDetailsViewController.swift
+//  MoneyHealth
+//
+//  Created by Alexander on 11/7/20.
+//
+
+import UIKit
+
+class SubscriptionDetailsViewController: UIViewController {
+    
+}
